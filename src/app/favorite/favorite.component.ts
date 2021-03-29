e7a37448a52282937b54ce402d0b8938be4fafb9
tillyninjaspace <squealingpet@gmail.com>
2021-03-28 18:27:09 -0700

    prepositions

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'favorite',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
-  isFavorite = false;
+ isFavorite = false;
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/title-case.pipe.ts
+++ src/app/title-case.pipe.ts
@@ -7,21 +7,29 @@ export class TitleCasePipe implements PipeTransform {
 
   transform(value: string): any {
     if (!value) return null;
-
-    let prepositions = [
-      'of',
-      'the'
-    ];
+   
     let words = value.split(' ');
     for (var index = 0; index < words.length; index++) {
-      if (index > 0 && prepositions.includes(words[index].toLowerCase()))
-        words[index] = words[index].toLowerCase();
+      let word = words[index]
+      if (index > 0 && this.isPreposition(word))
+        words[index] = word.toLowerCase();
         else  {
-          words[index] = words[index].substr(0,1).toUpperCase() + words[index].substr(1).toLowerCase()
+          words[index] = this.toTitleCase(word)
         }
     }
 
     return words.join(' ');
+  }
+  private toTitleCase(word: string): string {
+    return word.substr(0,1).toUpperCase() + word.substr(1).toLowerCase();
+  }
+
+  private isPreposition(word:string) : boolean {
+    let prepositions = [
+      'of',
+      'the'
+    ];
+    return prepositions.includes(word.toLowerCase())
   }
 
 }
